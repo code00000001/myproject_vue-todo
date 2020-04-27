2e3ea4ee7fc73f2ed78cade1a14afba15a2d0d8a
code00000001 <1976331463@qq.com>
2020-04-27 22:16:29 +0800

    【1.0.0】漏提交代码

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <!-- 组件MainHeader引入后就可以用在template的标签里了，命名是小驼峰加-的方式 -->
     <main-header></main-header>
+    <main-todo></main-todo>
   </div>
 </template>
 
@@ -10,6 +11,7 @@ import "./assets/styles/global.styl";
 
 //导入子组件vue模块
 import MainHeader from "./components/MainHeader.vue";
+import MainTodo from "./components/MainTodo/MainTodo.vue";
 
 export default {
   name: "App", //组件的名称
@@ -17,6 +19,8 @@ export default {
   components: {
     //组件名：组件对象
     MainHeader: MainHeader,
+    //导入组件的简写，只要组件名=组件对象
+    MainTodo,
   },
 };
 </script>
