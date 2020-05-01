3659a3dbf8430baf1a98e9efc08520ffd38f45a7
code00000001 <1976331463@qq.com>
2020-05-01 21:48:47 +0800

    【1.0.0】todo item样式设置

--- src/components/MainTodo/coms/TodoItem.vue
+++ src/components/MainTodo/coms/TodoItem.vue
@@ -13,10 +13,43 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  @import '~styles/theme.styl'
+  @import '~styles/mixins.styl'
+
 .todo-item
-  display flex
-  justify-content space-between
+  display: flex
+  justify-content: space-between
+  padding: 10px
+  font-size 24px
+
+  &:hover
+    button:after
+      content:'X'
+      font-size 24px
+      color $lightred
+
+  &.complated
+    label
+      color #d9d9dd
+      text-decoration line-through
+
+  input
+    width 50px
+    height 30px
+    text-align center
+    cleanDefaultStyle()
+    &:after
+      content: url('~images/unChecked.jpg')
+    &:checked:after
+      content:url('~images/Checked.jpg')
 
   label
     flex 1
+    transition color 0.4s
+
+  button
+    width 40px
+    background transparent
+    cleanDefaultStyle()
+    cursor pointer
 </style>
